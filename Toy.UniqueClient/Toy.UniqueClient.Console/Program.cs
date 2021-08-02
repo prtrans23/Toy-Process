a8d3e74e4e303494909843f2d3ea33c004595daf
rd-dhyoo <prtrans23@gmail.com>
2021-08-02 21:30:19 +0900

    Add Unique Action Program

---
+++ Toy.UniqueClient/Toy.UniqueClient.Console/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+
+namespace Toy.UniqueClient.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                var currentProcess = Process.GetCurrentProcess();
+                var myProgramName = currentProcess.ProcessName;
+
+                var processes = Process.GetProcesses();
+                var count = processes.Count(process => process.ProcessName == myProgramName);
+                var message = $"{myProgramName} is {count} count";
+
+                if (count > 1) 
+                {
+                    currentProcess.Kill();
+                }
+
+                Console.WriteLine(message);
+                Console.ReadLine();
+            }
+        }
+    }
+}
